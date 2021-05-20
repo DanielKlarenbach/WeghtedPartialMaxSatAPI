8ffeb8c3dba4fcaaab41ee5b3c19f4e67ea911ef
Daniel Klarenbach <daniel.klarenbach@gmail.com>
2021-05-20 13:51:50 +0200

    feat: implemented validate_and_return_wcnf

--- api/sat_based_wmps_solver.py
+++ api/sat_based_wmps_solver.py
@@ -1,5 +1,17 @@
-def validate_and_return_wcnf(wcnf):
-    pass
+import io
+
+from rest_framework.parsers import JSONParser
+
+from api.model_serializers import ClauseListSerializer
+
+
+def validate_and_return_wcnf(data):
+    stream = io.BytesIO(data)
+    wcnf = JSONParser().parse(stream)
+    serializer = ClauseListSerializer(data=wcnf)
+    serializer.is_valid(raise_exception=True)
+    return wcnf
+
 
 
 def wmp1(solver_name, formula):

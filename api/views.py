71fc58d3ee9668fcd89f9e86c26ce0c870fe7eca
Daniel Klarenbach <daniel.klarenbach@gmail.com>
2021-05-20 13:48:37 +0200

    refactor: called as_view() on WMPSSolverView in api.urls.py to correct the mistake

--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
 # Create your views here.
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from api.sat_based_wmps_solver import validate_and_return_wcnf
 
 
 class WPMSSolverView(APIView):
 
     def post(self, request, format=None):
-        pass
+        wcnf = validate_and_return_wcnf(request.data)
+        print(wcnf)
+        return Response()

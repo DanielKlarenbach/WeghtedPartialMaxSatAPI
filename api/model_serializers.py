c04195e5389326d68a1ea67c1538738002d47350
Daniel Klarenbach <daniel.klarenbach@gmail.com>
2021-05-20 16:38:28 +0200

    feat: Added create methods for ClauseSerializer and ClauseListSerializer

--- api/model_serializers.py
+++ api/model_serializers.py
@@ -1,13 +1,27 @@
 from rest_framework import serializers
 
+from api.models import Clause
+
 
 class ClauseSerializer(serializers.Serializer):
-    variables = serializers.IntegerField(
-        many=True,
+    variables = serializers.ListSerializer(
+        child=serializers.IntegerField(),
         allow_empty=False
     )
     weight = serializers.IntegerField(min_value=0)
 
+    def create(self, validated_data):
+        return Clause(**validated_data)
+
 
 class ClauseListSerializer(serializers.Serializer):
     clauses = ClauseSerializer(many=True, allow_empty=False)
+
+    def create(self, validated_data):
+        clauses = []
+        for data in validated_data['clauses']:
+            serializer = ClauseSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            clause = serializer.save()
+            clauses.append(clause)
+        return clauses

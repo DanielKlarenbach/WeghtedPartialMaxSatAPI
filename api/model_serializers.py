af03db3db822ef6f1c17f9df1f1443096ec205bc
Daniel Klarenbach <daniel.klarenbach@gmail.com>
2021-05-20 12:14:27 +0200

    test: implemented tests for validate_and_return_wcnf function

--- api/model_serializers.py
+++ api/model_serializers.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 
 
 class ClauseSerializer(serializers.Serializer):
-    variables = serializers.ListField(
-        child=serializers.IntegerField()
+    variables = serializers.IntegerField(
+        many=True,
+        allow_empty=False
     )
     weight = serializers.IntegerField(min_value=0)
+
+
+class ClauseListSerializer(serializers.Serializer):
+    clauses = ClauseSerializer(many=True, allow_empty=False)
--- api/tests.py
+++ api/tests.py
@@ -1,1 +1,56 @@
 # Create your tests here.
+import json
+
+from django.test import TestCase
+from rest_framework.exceptions import ValidationError
+
+from api.sat_based_wmps_solver import validate_and_return_wcnf
+
+
+class ValidateAndReturnWCNFTestCase(TestCase):
+
+    def test_validate_and_return_wcnf_with_valid_clause_list(self):
+        clauses_data = [([1], 1), ([2], 2), ([3], 3), ([-1, -2], 0), ([1, -3], 0), ([2, -3], 0)]
+        clauses = [{'variables': clause_data[0], 'weight': clause_data[1]} for clause_data in clauses_data]
+        request_data = json.dumps(clauses)
+
+        result_clauses = validate_and_return_wcnf(request_data)
+
+        self.assertListEqual(result_clauses, clauses)
+
+    def test_validate_and_return_wcnf_with_empty_clause_list(self):
+        request_data = json.dumps([])
+
+        self.assertRaises(ValidationError, validate_and_return_wcnf, request_data)
+
+    def test_validate_and_return_wcnf_with_clause_with_invalid_variable_type(self):
+        clause_with_invalid_variable_type = ([2.5213], 2)
+        clauses_data = [([1], 1), clause_with_invalid_variable_type, ([3], 3), ([-1, -2], 0), ([1, -3], 0), ([2, -3], 0)]
+        clauses = [{'variables': clause_data[0], 'weight': clause_data[1]} for clause_data in clauses_data]
+        request_data = json.dumps(clauses)
+
+        self.assertRaises(ValidationError, validate_and_return_wcnf, request_data)
+
+    def test_validate_and_return_wcnf_with_clause_with_invalid_weight_type(self):
+        clause_with_invalid_weight_type = ([2], "dsadasdsa")
+        clauses_data = [([1], 1), clause_with_invalid_weight_type, ([3], 3), ([-1, -2], 0), ([1, -3], 0), ([2, -3], 0)]
+        clauses = [{'variables': clause_data[0], 'weight': clause_data[1]} for clause_data in clauses_data]
+        request_data = json.dumps(clauses)
+
+        self.assertRaises(ValidationError, validate_and_return_wcnf, request_data)
+
+    def test_validate_and_return_wcnf_with_clause_with_empty_variable_list(self):
+        clause_with_empty_variable_list = ([], 2)
+        clauses_data = [([1], 1), clause_with_empty_variable_list , ([3], 3), ([-1, -2], 0), ([1, -3], 0), ([2, -3], 0)]
+        clauses = [{'variables': clause_data[0], 'weight': clause_data[1]} for clause_data in clauses_data]
+        request_data = json.dumps(clauses)
+
+        self.assertRaises(ValidationError, validate_and_return_wcnf, request_data)
+
+    def test_validate_and_return_wcnf_clause_with_no_weight(self):
+        clause_with_no_weight = ([3])
+        clauses_data = [([1], 1), ([2], 2),clause_with_no_weight, ([-1, -2], 0), ([1, -3], 0), ([2, -3], 0)]
+        clauses = [{'variables': clause_data[0], 'weight': None if len(clause_data)==1 else clause_data[1]} for clause_data in clauses_data]
+        request_data = json.dumps(clauses)
+
+        self.assertRaises(ValidationError, validate_and_return_wcnf, request_data)
